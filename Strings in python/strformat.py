fb7cf1cef5c51255f3c6d98658cd9cafe84aa64d
Lipsita <lipsitakhadgarai16@gmail.com>
2026-06-23 15:36:09 +0530

    23/06/26

---
+++ Strings in python/strformat.py
@@ -0,0 +1,10 @@
+pi = 2.14159265
+print(f"Pi rounded to 2 decimal places  {pi : .2f}")
+for i in range(20):
+ text = "Python"
+ print(f"{text:<10}")   # Left align
+ print(f"{text:^10}")   # Center align
+ print(f"{text:>10}")   # Right align
+ print(f"{text:^10}")   # Center align
+ print(f"{text:<10}")   # Left align
+
